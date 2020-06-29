95b08ae29ecbe57cfb32b93a54395257e1008b66
Taylor Bromley <taylorbromley@sky.com>
2020-06-29 19:32:34 +0100

    Added rocket movement (plus fixes to rotation and collision issues) and audio with fade effect

---
+++ Assets/Scripts/FadeAudioSource.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class FadeAudioSource
+{
+    public static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float start = audioSource.volume;
+
+        yield return new WaitForSeconds(0.2f);
+
+        while (currentTime < duration)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                audioSource.volume = 1.0f;
+                yield break;
+            }
+            currentTime += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);            
+            yield return null;
+        }
+        audioSource.Stop();
+        yield break;
+    }
+}
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -5,72 +5,84 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    bool right = false; //
+    bool right = false; //Movement condition to ensure there isn't a prioritised direction when both arrow (left & right) keys are down at once
     Rigidbody rb;
-   
+    AudioSource audioSource;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    //Rocket movement buttons
-    private void ProcessInput()
+
+    //Rocket thrust button
+    private void Thrust()
     {
-        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.Space)) //Both right and left down while boosting
+        if (Input.GetKey(KeyCode.Space))  //Boosting
         {
-            if (right)  //Checks bool of first arrow pressed
-            {
-                print("Rotating right with thrust");
-            }
-            else
-            {
-                print("Rotating left with thrust");
-            }
-        }
-        else if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.Space))   //Both right and left down while NOT boosting
-        {
-            if (right)  //Checks bool of first arrow pressed
-            {
-                print("Rotating right");
-            }
-            else
-            {
-                print("Rotating left");
-            }
-        }
-        else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.Space))   //Right down while boosting
-        {
-            right = true;            
-            print("Rotating right with thrust");
-        }
-        else if (!Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.Space))   //Left down while boosting
-        {
-            right = false;            
-            print("Rotating left with thrust");
-        }
-        else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.Space))  //Right down
-        {
-            right = true;            
-            print("Rotating right");
-        }
-        else if (!Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.Space))  //Left down
-        {
-            right = false;            
-            print("Rotating left");
-            //rb.AddRelativeForce
-        }        
-        else if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.Space))  //Boosting
-        {
-            print("Thrusting");
             rb.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) //So the audio doesn't layer itself
+            {
+                audioSource.volume = 1.0f;
+                audioSource.Play();
+            }
         }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            StartCoroutine(FadeAudioSource.StartFade(audioSource, 0.5f, 0.0f)); //Starts a coroutine to start volume fade as long as Space hasn't been presses for x amount of time otherwise the volume will reset and stop fading.
+        }
+    }
+
+
+    //Rocket movement buttons
+    private void Rotate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))  //Just right
+        {
+            right = true;
+            transform.Rotate(Vector3.back);
+        }
+        else if (!Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow))  //Just left
+        {
+            right = false;
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.LeftArrow))    //Both right and left
+        {
+            if (right)  //Checks bool of first arrow pressed
+            {
+                transform.Rotate(Vector3.back);
+            }
+            else
+            {
+                transform.Rotate(Vector3.forward);
+            }
+        }
+    }
+
+
+    //Stops angular velocity from collisions messing with the rotation movement forever after
+    private void OnCollisionExit(Collision collision)
+    {
+        rb.angularVelocity = Vector3.zero;        
+    }
+
+    //Stops the y rotation from changing on collisions (even though the rb rotation contraint has been set)
+    private void OnCollisionStay(Collision collision)
+    {
+        float z = transform.eulerAngles.z;
+        float x = transform.eulerAngles.x;        
+        transform.rotation = Quaternion.Euler(x, 0, z);
     }
 }
